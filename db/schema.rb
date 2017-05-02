f1df09cbe45f3a3ae6ec4ed99c3fab2defbea2cb
Unknown <sebastin25@gmail.com>
2017-05-01 20:46:04 -0600

    6

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,10 @@ ActiveRecord::Schema.define(version: 20170327021151) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password"
+    t.string   "password_digest"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
